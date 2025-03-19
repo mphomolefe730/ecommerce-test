e7b1236d6b25630081c83600f42005e3715a5f2f
mphomolefe730 <mphomolefe730@gmail.com>
2025-03-19 19:59:14 +0200

    fix environment file

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-	renderApiLink: 'http://localhost:3000',
-	//renderApiLink: 'https://ecommerce-test-api-production.up.railway.app',
+	//renderApiLink: 'http://localhost:3000',
+	renderApiLink: 'https://ecommerce-test-api-production.up.railway.app',
         frontendLink: 'https://ecommerce-test-production-1b0f.up.railway.app',
 	firebaseConfig: {
 		apiKey: "AIzaSyC0pOtd-Zt9qLuPbRDyAMgsWdkP978MjE8",
