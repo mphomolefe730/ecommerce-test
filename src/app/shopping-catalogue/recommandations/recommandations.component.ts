3cf95fc23e59047681120dd9691766bfa8213623
mphomolefe730 <mphomolefe730@gmail.com>
2025-03-21 12:19:00 +0200

    add styling for recommendations, add input tag to recieve from parent component, add function to view other products

--- src/app/shopping-catalogue/recommandations/recommandations.component.ts
+++ src/app/shopping-catalogue/recommandations/recommandations.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recommandations',
   templateUrl: './recommandations.component.html',
   styleUrls: ['./recommandations.component.scss']
 })
-export class RecommandationsComponent {
+
+export class RecommandationsComponent implements OnInit, OnChanges {
+  @Input() recommendations:any[] = [];
+
+  constructor(
+    private productService:ProductService,
+    private router:Router
+  ){};
+
+  ngOnInit():void{
+   // console.log("oninit" + this.recommendations);
+  };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['recommendations']) {
+      //console.log(this.recommendations);
+    }
+  };
+  
+  async viewProduct(productId:string,productName:string){
+    this.productService.addToRecentlyViewedProduct(productId);
+    const productNameFormated = productName.split(' ').join('-');
+    this.router.navigate([`product/${productNameFormated}/pd/${productId}`]);
+  }
 
 }
