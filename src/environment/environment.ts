649c45f82938a487411c6f01f39295395e664511
Mpho Molefe <117911388+mphomolefe730@users.noreply.github.com>
2025-06-07 21:38:02 +0200

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	//renderApiLink: 'http://localhost:3000',
-	renderApiLink: 'https://ecommerce-test-api-i2se.onrender.com/',
+	renderApiLink: 'https://ecommerce-test-api-i2se.onrender.com',
   frontendLink: 'https://ecommerce-test-production-1b0f.up.railway.app',
 	firebaseConfig: {
 		apiKey: "AIzaSyC0pOtd-Zt9qLuPbRDyAMgsWdkP978MjE8",
