8cc6f49ceb851f2d3e914c4e754cae49382c421c
mphomolefe730 <mphomolefe730@gmail.com>
2026-04-13 18:19:21 +0200

    add service yaml for kubernetes and workflow for automatic deployment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,14 +1,1 @@
-export const environment = {
-	//renderApiLink: 'http://localhost:3000',
-	renderApiLink: 'https://ecommerce-test-api-i2se.onrender.com',
-  frontendLink: 'https://ecommerce-test-production-1b0f.up.railway.app',
-	firebaseConfig: {
-		apiKey: "AIzaSyC0pOtd-Zt9qLuPbRDyAMgsWdkP978MjE8",
-		authDomain: "ecommerce-connect-c7850.firebaseapp.com",
-		projectId: "ecommerce-connect-c7850",
-		storageBucket: "ecommerce-connect-c7850.appspot.com",
-		messagingSenderId: "918668314559",
-		appId: "1:918668314559:web:165c2f153bbba1d217c30a",
-		measurementId: "G-MB6S41P1YV"
-	},
-}
+export const environment = {                                                                                                    //renderApiLink: 'http://localhost:3000',                                                                               renderApiLink: 'https://172.26.226.200:3000',                                                                           frontendLink: 'https://172.26.226.200:3000',                                                                            firebaseConfig: {                                                                                                               apiKey: "AIzaSyC0pOtd-Zt9qLuPbRDyAMgsWdkP978MjE8",                                                                      authDomain: "ecommerce-connect-c7850.firebaseapp.com",                                                                  projectId: "ecommerce-connect-c7850",                                                                                   storageBucket: "ecommerce-connect-c7850.appspot.com",                                                                   messagingSenderId: "918668314559",                                                                                      appId: "1:918668314559:web:165c2f153bbba1d217c30a",                                                                     measurementId: "G-MB6S41P1YV"                                                                                   },                                                                                                              }  
